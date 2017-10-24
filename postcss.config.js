29c2c4dbfe558b6a596ed546e91ec5844a51a9b1
winjeysong <winjey-song@163.com>
2017-10-24 21:07:40 +0800

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
+/**
+ * @ postcss config with autoprefixer
+ */
+
 const autoPreFixer = require("autoprefixer");
 
 module.exports = {
     plugins: [
-        autoPreFixer
+        autoPreFixer({
+            browsers: [
+                ">1%",
+                "last 4 versions",
+                "Firefox ESR",
+                "not ie < 9" // React doesn"t support IE8 anyway
+            ]
+        })
     ]
 };
 
